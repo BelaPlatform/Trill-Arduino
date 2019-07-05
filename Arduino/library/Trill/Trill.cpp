308b09b08d169bf70dcea40cf4b3493203000d64
adanl.benito <adanl.benito@gmail.com>
2019-07-05 15:55:59 +0100

    Arduino library expanded for 2D sliders

--- Arduino/library/Trill/Trill.cpp
+++ Arduino/library/Trill/Trill.cpp
@@ -1,6 +1,6 @@
 /*****************************
  * Trill library for Arduino
- * (c) 2019 Andrew McPherson 
+ * (c) 2019 Andrew McPherson
  *
  * This library communicates with the Trill sensors
  * using I2C.
@@ -10,12 +10,14 @@
 
 #include "Trill.h"
 
+#define MAX_TOUCH_1D_OR_2D ((device_type_ == TRILL_DEVICE_2D ? kMaxTouchNum2D : kMaxTouchNum1D))
+
 Trill::Trill(uint8_t i2c_address, uint8_t reset_pin)
 : i2c_address_(i2c_address), reset_pin_(reset_pin),
   device_type_(TRILL_DEVICE_NONE), firmware_version_(0),
   mode_(0xFF), last_read_loc_(0xFF), num_touches_(0),
   raw_bytes_left_(0)
-{	
+{
 }
 
 /* Initialise the hardware. Returns the type of device attached, or 0
@@ -26,13 +28,16 @@ int Trill::begin() {
 		pinMode(reset_pin_, OUTPUT);
 		digitalWrite(reset_pin_, LOW);
 	}
-	
+
 	/* Start I2C */
 	Wire.begin();
-	
+
 	/* Put the device (assuming it exists) in normal mode */
 	setMode(TRILL_MODE_NORMAL);
-	
+
+	/* Wait to process the command before sending the second identify command */
+	delay(25);
+
 	/* Check the type of device attached */
 	return identify();
 }
@@ -43,49 +48,73 @@ int Trill::identify() {
 	Wire.write(kOffsetCommand);
 	Wire.write(kCommandIdentify);
 	Wire.endTransmission();
-	
+
+	/* Give Trill time to process this command */
+	delay(25);
+
 	last_read_loc_ = kOffsetCommand;
-	Wire.requestFrom(i2c_address_, (uint8_t)2);
-	
-	if(Wire.available() < 2) {
+	Wire.requestFrom(i2c_address_, (uint8_t)3);
+
+	if(Wire.available() < 3) {
 		/* Unexpected or no response; no valid device connected */
 		device_type_ = TRILL_DEVICE_NONE;
 		firmware_version_ = 0;
 		return device_type_;
 	}
-	
+
+	Wire.read();	// Discard first input
 	device_type_ = Wire.read();
 	firmware_version_ = Wire.read();
-	
+
+	return device_type_;
+}
+
+/* Return the type of device attached gathered from a previous call to identify() */
+int Trill::deviceType() {
 	return device_type_;
 }
 
 /* Read the latest scan value from the sensor. Returns true on success. */
 boolean Trill::read() {
 	uint8_t loc = 0;
-	
+	uint8_t length = kNormalLengthDefault;
+
 	/* Set the read location to the right place if needed */
 	prepareForDataRead();
-	
-	Wire.requestFrom(i2c_address_, (uint8_t)kNormalLengthDefault);
+
+	if(device_type_ == TRILL_DEVICE_2D)
+		length = kNormalLength2D;
+
+	Wire.requestFrom(i2c_address_, length);
 	while(Wire.available()) {
 		buffer_[loc++] = Wire.read();
 	}
-	
+
 	/* Check for read error */
-	if(loc < kNormalLengthDefault) {
+	if(loc < length) {
 		num_touches_ = 0;
 		return false;
 	}
-	
+
 	/* Look for the first instance of 0xFFFF (no touch)
    	   in the buffer */
-	for(loc = 0; loc < kMaxTouchNum; loc++) {
+	for(loc = 0; loc < MAX_TOUCH_1D_OR_2D; loc++) {
 		if(buffer_[2 * loc] == 0xFF && buffer_[2 * loc + 1] == 0xFF)
 			break;
 	}
 	num_touches_ = loc;
-	
+
+	if(device_type_ == TRILL_DEVICE_2D) {
+		/* Look for the number of horizontal touches in 2D sliders
+		   which might be different from number of vertical touches */
+		for(loc = 0; loc < kMaxTouchNum2D; loc++) {
+			if(buffer_[2 * loc + 4 * kMaxTouchNum2D] == 0xFF && 
+			   buffer_[2 * loc + 4 * kMaxTouchNum2D + 1] == 0xFF)
+				break;
+		}
+		num_touches_ |= (loc << 4);
+	}
+
 	return true;
 }
 
@@ -95,8 +124,8 @@ void Trill::updateBaseline() {
 	Wire.write(kOffsetCommand);
 	Wire.write(kCommandBaselineUpdate);
 	Wire.endTransmission();
-	
-	last_read_loc_ = kOffsetCommand;	
+
+	last_read_loc_ = kOffsetCommand;
 }
 
 /* Reset the sensor, if reset pin is enabled */
@@ -113,7 +142,20 @@ void Trill::reset() {
 int Trill::numberOfTouches() {
 	if(mode_ != TRILL_MODE_NORMAL)
 		return 0;
-	return num_touches_;
+
+	/* Lower 4 bits hold number of 1-axis or vertical touches */
+	return (num_touches_ & 0x0F);
+}
+
+/* How many horizontal touches for 2D? */
+int Trill::numberOfHorizontalTouches() {
+	if(mode_ != TRILL_MODE_NORMAL)
+		return 0;
+	if(device_type_ != TRILL_DEVICE_2D)
+		return 0;
+
+	/* Upper 4 bits hold number of horizontal touches */
+	return (num_touches_ >> 4);
 }
 
 /* Location and size of a particular touch, ranging from 0 to N-1.
@@ -122,13 +164,13 @@ int Trill::touchLocation(uint8_t touch_num) {
 	int result;
 
 	if(mode_ != TRILL_MODE_NORMAL)
-		return -1;	
-	if(touch_num >= kMaxTouchNum)
 		return -1;
-	
+	if(touch_num >= MAX_TOUCH_1D_OR_2D)
+		return -1;
+
 	result = buffer_[2*touch_num] * 256;
 	result += buffer_[2*touch_num + 1];
-	
+
 	return result;
 }
 
@@ -136,33 +178,68 @@ int Trill::touchSize(uint8_t touch_num) {
 	int result;
 
 	if(mode_ != TRILL_MODE_NORMAL)
-		return -1;	
-	if(touch_num >= kMaxTouchNum)
 		return -1;
-	
-	result = buffer_[2*touch_num + 2*kMaxTouchNum] * 256;
-	result += buffer_[2*touch_num + 2*kMaxTouchNum + 1];
-	
+	if(touch_num >= MAX_TOUCH_1D_OR_2D)
+		return -1;
+
+	result = buffer_[2*touch_num + 2*MAX_TOUCH_1D_OR_2D] * 256;
+	result += buffer_[2*touch_num + 2*MAX_TOUCH_1D_OR_2D + 1];
+
+	return result;
+}
+
+/* These methods for horizontal touches on 2D sliders */
+int Trill::touchHorizontalLocation(uint8_t touch_num) {
+	int result;
+
+	if(mode_ != TRILL_MODE_NORMAL)
+		return -1;
+	if(device_type_ != TRILL_DEVICE_2D)
+		return -1;
+	if(touch_num >= kMaxTouchNum2D)
+		return -1;
+
+	result = buffer_[2*touch_num + 4*kMaxTouchNum2D] * 256;
+	result += buffer_[2*touch_num + 4*kMaxTouchNum2D + 1];
+
+	return result;
+}
+
+int Trill::touchHorizontalSize(uint8_t touch_num) {
+	int result;
+
+	if(mode_ != TRILL_MODE_NORMAL)
+		return -1;
+	if(device_type_ != TRILL_DEVICE_2D)
+		return -1;
+	if(touch_num >= kMaxTouchNum2D)
+		return -1;
+
+	result = buffer_[2*touch_num + 6*kMaxTouchNum2D] * 256;
+	result += buffer_[2*touch_num + 6*kMaxTouchNum2D + 1];
+
 	return result;
 }
 
 /* Request raw data; wrappers for Wire */
 void Trill::requestRawData(uint8_t max_length) {
 	uint8_t length;
-	
+
 	prepareForDataRead();
-	
+
 	if(max_length == 0xFF) {
 		/* Use default for the particular device if no length specified */
 		if(device_type_ == TRILL_DEVICE_1D)
 			length = kRawLength1D;
+		else if(device_type_ == TRILL_DEVICE_2D)
+			length = kRawLength2D;
 		else
 			length = kRawLengthDefault;
 	}
 	if(length > kRawLengthMax)
 		length = kRawLengthMax;
-	
-	/* The raw data might be longer than the Wire.h maximum buffer 
+
+	/* The raw data might be longer than the Wire.h maximum buffer
 	 * (BUFFER_LENGTH in Wire.h).
 	 * If so, split it into two reads. */
 	if(length <= BUFFER_LENGTH) {
@@ -183,7 +260,7 @@ int Trill::rawDataAvailable() {
 /* Raw data is in 16-bit big-endian format */
 int Trill::rawDataRead() {
 	int result;
-	
+
 	if(Wire.available() < 2) {
 		/* Read more bytes if we need it */
 		if(raw_bytes_left_ > 0) {
@@ -192,17 +269,17 @@ int Trill::rawDataRead() {
 			Wire.write(kOffsetData + BUFFER_LENGTH);
 			Wire.endTransmission();
 			last_read_loc_ = kOffsetData + BUFFER_LENGTH;
-			
+
 			/* Now gather what's left */
 			Wire.requestFrom(i2c_address_, raw_bytes_left_);
-			raw_bytes_left_ = 0;		
+			raw_bytes_left_ = 0;
 		}
 
 		/* Check again if we've got anything... */
 		if(Wire.available() < 2)
 			return 0;
 	}
-	
+
 	result = (int)Wire.read() * 256;
 	result += (int)Wire.read();
 	return result;
@@ -215,9 +292,9 @@ void Trill::setMode(uint8_t mode) {
 	Wire.write(kCommandMode);
 	Wire.write(mode);
 	Wire.endTransmission();
-	
+
 	mode_ = mode;
-	last_read_loc_ = kOffsetCommand;		
+	last_read_loc_ = kOffsetCommand;
 }
 
 void Trill::setScanSettings(uint8_t speed, uint8_t num_bits) {
@@ -227,8 +304,8 @@ void Trill::setScanSettings(uint8_t speed, uint8_t num_bits) {
 	Wire.write(speed);
 	Wire.write(num_bits);
 	Wire.endTransmission();
-	
-	last_read_loc_ = kOffsetCommand;		
+
+	last_read_loc_ = kOffsetCommand;
 }
 
 void Trill::setPrescaler(uint8_t prescaler) {
@@ -237,8 +314,8 @@ void Trill::setPrescaler(uint8_t prescaler) {
 	Wire.write(kCommandPrescaler);
 	Wire.write(prescaler);
 	Wire.endTransmission();
-	
-	last_read_loc_ = kOffsetCommand;			
+
+	last_read_loc_ = kOffsetCommand;
 }
 
 void Trill::setNoiseThreshold(uint8_t threshold) {
@@ -247,8 +324,8 @@ void Trill::setNoiseThreshold(uint8_t threshold) {
 	Wire.write(kCommandNoiseThreshold);
 	Wire.write(threshold);
 	Wire.endTransmission();
-	
-	last_read_loc_ = kOffsetCommand;		
+
+	last_read_loc_ = kOffsetCommand;
 }
 
 void Trill::setIDACValue(uint8_t value) {
@@ -257,8 +334,8 @@ void Trill::setIDACValue(uint8_t value) {
 	Wire.write(kCommandIdac);
 	Wire.write(value);
 	Wire.endTransmission();
-	
-	last_read_loc_ = kOffsetCommand;		
+
+	last_read_loc_ = kOffsetCommand;
 }
 
 void Trill::setMinimumTouchSize(uint16_t size) {
@@ -268,8 +345,8 @@ void Trill::setMinimumTouchSize(uint16_t size) {
 	Wire.write(size >> 8);
 	Wire.write(size & 0xFF);
 	Wire.endTransmission();
-	
-	last_read_loc_ = kOffsetCommand;		
+
+	last_read_loc_ = kOffsetCommand;
 }
 
 /* Prepare the device to read data if it is not already prepared */
@@ -280,5 +357,5 @@ void Trill::prepareForDataRead() {
 		Wire.endTransmission();
 
 		last_read_loc_ = kOffsetData;
-	}		
+	}
 }
--- Arduino/library/Trill/Trill.h
+++ Arduino/library/Trill/Trill.h
@@ -1,6 +1,6 @@
 /*****************************
  * Trill library for Arduino
- * (c) 2019 Andrew McPherson 
+ * (c) 2019 Andrew McPherson
  *
  * This library communicates with the Trill sensors
  * using I2C.
@@ -25,6 +25,7 @@
 
 #define TRILL_DEVICE_NONE	0
 #define TRILL_DEVICE_1D		1
+#define TRILL_DEVICE_2D		2
 
 #define TRILL_SPEED_ULTRA_FAST 0
 #define TRILL_SPEED_FAST	   1
@@ -35,42 +36,51 @@
 class Trill {
 public:
 	Trill(uint8_t i2c_address, uint8_t reset_pin = 0);
-	
+
 	/* Initialise the hardware */
 	int begin();
-	
+
 	/* --- Main communication --- */
-	
+
 	/* Return the type of device attached, or 0 if none is attached. 
 	   Same as begin(), but without re-initialising the system. */
 	int identify();
-	
+
+	/* Return the device type already identified */
+	int deviceType();
+
 	/* Read the latest scan value from the sensor. Returns true on success. */
 	boolean read();
-	
+
 	/* Update the baseline value on the sensor */
 	void updateBaseline();
-	
+
 	/* Reset the sensor, if reset pin is enabled */
 	void reset();
 
 	/* --- Data processing --- */
-	
+
 	/* How many touches? < 0 means error. */
 	int numberOfTouches();
-	
+	/* How many horizontal touches for 2D? */
+	int numberOfHorizontalTouches();
+
 	/* Location and size of a particular touch, ranging from 0 to N-1.
 	   Returns -1 if no such touch exists. */
 	int touchLocation(uint8_t touch_num);
 	int touchSize(uint8_t touch_num);
-	
+
+	/* These methods for horizontal touches on 2D sliders */
+	int touchHorizontalLocation(uint8_t touch_num);
+	int touchHorizontalSize(uint8_t touch_num);
+
 	/* --- Raw data handling --- */
-	
+
 	/* Request raw data; wrappers for Wire */
 	void requestRawData(uint8_t max_length = 0xFF);
 	int rawDataAvailable();
 	int rawDataRead();
-	
+
 	/* --- Scan configuration settings --- */
 	void setMode(uint8_t mode);
 	void setScanSettings(uint8_t speed, uint8_t num_bits);
@@ -78,10 +88,10 @@ public:
 	void setNoiseThreshold(uint8_t threshold);
 	void setIDACValue(uint8_t value);
 	void setMinimumTouchSize(uint16_t size);
-	
+
 private:
 	void prepareForDataRead();	
-	
+
 	enum {
 		kCommandNone = 0,
 		kCommandMode = 1,
@@ -93,22 +103,25 @@ private:
 		kCommandMinimumSize = 7,
 		kCommandIdentify = 255
 	};
-	
+
 	enum {
 		kOffsetCommand = 0,
 		kOffsetData = 4
 	};
-	
+
 	enum {
 		kNormalLengthDefault = 20,
 		kNormalLengthMax = 20,
-		kMaxTouchNum = 5,
+		kNormalLength2D = 32,
+		kMaxTouchNum1D = 5,
+		kMaxTouchNum2D = 4,
 		
 		kRawLength1D = 52,
+		kRawLength2D = 60,
 		kRawLengthDefault = 60,
 		kRawLengthMax = 60
 	};
-	
+
 	uint8_t i2c_address_;	/* Address of this slider on I2C bus */
 	uint8_t reset_pin_;		/* Pin for active-high reset of sensor */
 	uint8_t device_type_;	/* Which type of device is connected, if any */
