b7dbd2f5b21f40eab5209bd37cd532041351663d
Robert Jack <robert.h.jack@gmail.com>
2019-07-05 14:23:19 +0100

    Added to Arduino Library, Trill Square Display.

---
+++ Arduino/examples/Trill_square_display/Trill_square_display.ino
@@ -0,0 +1,53 @@
+#include <Wire.h>
+#include <Trill.h>
+
+Trill slider(24);
+boolean touchActive = false;
+
+void setup() {
+  // Initialise serial and touch sensor
+  Serial.begin(115200);
+  
+  int deviceType = slider.begin();
+  
+  if(!deviceType)
+    Serial.println("failed to initialise slider");  
+//  else if(deviceType != CAPSLIDER_DEVICE_2D)
+    Serial.println("attached device is not a 2D slider");
+    
+  slider.setMode(TRILL_DEVICE_2D);
+}
+
+void loop() {  
+  // Read 20 times per second
+  delay(50);
+  slider.read();
+  
+  if(slider.numberOfTouches() > 0) {
+    Serial.print(slider.numberOfTouches());
+    Serial.print(" ");
+    Serial.print(slider.numberOfHorizontalTouches());
+    Serial.print(" ");
+    
+    for(int i = 0; i < slider.numberOfTouches(); i++) {
+        Serial.print(slider.touchLocation(i));
+        Serial.print(" ");
+        Serial.print(slider.touchSize(i));
+        Serial.print(" ");
+    }
+    for(int i = 0; i < slider.numberOfHorizontalTouches(); i++) {
+        Serial.print(slider.touchHorizontalLocation(i));
+        Serial.print(" ");
+        Serial.print(slider.touchHorizontalSize(i));
+        Serial.print(" ");
+    }
+    
+    Serial.println("");
+    touchActive = true;
+  }
+  else if(touchActive) {
+    // Print a single line when touch goes off
+    Serial.println("0 0");   
+    touchActive = false; 
+  }
+}
