b07052a8bc80d99de8e0022e176f5edb3a6b4dda
adanlbenito <adanl.benito@gmail.com>
2020-05-30 13:10:01 +0100

    Updated raw reading examples

--- examples/Arduino/Trill_raw/Trill_raw.ino
+++ examples/Arduino/Trill_raw/Trill_raw.ino
@@ -1,29 +1,33 @@
 
 #include <Trill.h>
 
-Trill slider(0x30); // for Trill Craft
+Trill trillSensor; // for Trill Craft
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  
-  if(!slider.begin())
-    Serial.println("failed to initialise slider");
-   
-  slider.setMode(Trill::DIFF);
-  //delay(10);
-  //slider.setIDACValue(160);
+  int ret = trillSensor.begin(Trill::TRILL_CRAFT);
+  if(ret != 0) {
+    Serial.println("failed to initialise trillSensor");
+    Serial.print("Error code: ");
+    Serial.println(ret);
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
   delay(100);
-  slider.requestRawData();
+  trillSensor.requestRawData();
    
-  if(slider.rawDataAvailable() > 0) {
-    while(slider.rawDataAvailable() > 0) {
-      int data = slider.rawDataRead();
+  if(trillSensor.rawDataAvailable() > 0) {
+    while(trillSensor.rawDataAvailable() > 0) {
+      int data = trillSensor.rawDataRead();
+      if(data < 1000)
+        Serial.print(0);
+      if(data < 100)
+        Serial.print(0);
+      if(data < 10)
+        Serial.print(0);
       Serial.print(data);
       Serial.print(" ");
     }
--- examples/Arduino/Trill_raw_multi/Trill_raw_multi.ino
+++ examples/Arduino/Trill_raw_multi/Trill_raw_multi.ino
@@ -7,7 +7,7 @@ The I2C pins of all the sensors are connected together (SCL with SCL, SDA with S
 Each sensor has a unique address. Here is the table of address for each sensor type,
 the first column is the default address:
 
-| Type:  | Addresses (hex)                              |
+| Type:  | Addresses (trillHex)                              |
 |--------|----------------------------------------------|
 | BAR    | 0x20 0x21 0x22 0x23 0x24 0x25 0x26 0x27 0x28 |
 | SQUARE | 0x28 0x29 0x2A 0x2B 0x2C 0x2D 0x2E 0x2F 0x30 |
@@ -15,86 +15,101 @@ the first column is the default address:
 | RING   | 0x38 0x39 0x3A 0x3B 0x3C 0x3D 0x3E 0x3F 0x40 |
 | HEX    | 0x40 0x41 0x42 0x43 0x44 0x45 0x46 0x47 0x48 |
 
-In this example the sensor readings are printed to the console as raw values using the 
+In this example the sensor readings are printed to the console as raw values using the
 `rawDataRead()` method.
 
 */
 
-Trill craft(0x30); // for Trill Craft
-Trill squareTrill(0x28); // for Trill squareTrill
-Trill bar(0x20); // for Trill Bar
-Trill hex(0x40); // for Trill Hex
-
-
+Trill trillBar; // for Trill Bar
+Trill trillSquare; // for Trill Square
+Trill trillCraft; // for Trill Craft
+Trill trillRing; // for Trill Ring
+Trill trillHex; // for Trill Hex
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  
-  if(!craft.begin())
-    Serial.println("failed to initialise craft");
 
-  if(!squareTrill.begin())
-    Serial.println("failed to initialise squareTrill");
+  if(trillBar.begin(Trill::TRILL_BAR) != 0)
+    Serial.println("failed to initialise trill bar");
 
-  if(!bar.begin())
-    Serial.println("failed to initialise bar");
+  if(trillSquare.begin(Trill::TRILL_SQUARE) != 0)
+    Serial.println("failed to initialise trill square");
 
-  if(!hex.begin())
-    Serial.println("failed to initialise hex");
-   
-  craft.setMode(Trill::DIFF);
-  squareTrill.setMode(Trill::DIFF);
-  bar.setMode(Trill::DIFF);
-  hex.setMode(Trill::DIFF);
+  if(trillCraft.begin(Trill::TRILL_CRAFT) != 0)
+    Serial.println("failed to initialise trill craft");
+
+  if(trillRing.begin(Trill::TRILL_RING) != 0)
+    Serial.println("failed to initialise trill ring");
+
+  if(trillHex.begin(Trill::TRILL_HEX) != 0)
+    Serial.println("failed to initialise trill hex");
+
+  trillBar.setMode(Trill::DIFF);
+  trillSquare.setMode(Trill::DIFF);
+  trillCraft.setMode(Trill::DIFF);
+  trillRing.setMode(Trill::DIFF);
+  trillHex.setMode(Trill::DIFF);
+}
+
+void printLine(unsigned int n, char character = '_') {
+  for(int i = 0; i < n; i++) {
+    Serial.print(character);
+  }
+  Serial.println("");
+}
+
+void printRawData(Trill & trill) {
+  while(trill.rawDataAvailable() > 0) {
+    int data = trill.rawDataRead();
+    if(data < 1000)
+      Serial.print(0);
+    if(data < 100)
+      Serial.print(0);
+    if(data < 10)
+      Serial.print(0);
+    Serial.print(data);
+    Serial.print(" ");
+  }
+  Serial.println("");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
   delay(100);
-  craft.requestRawData();
-  squareTrill.requestRawData();
-  bar.requestRawData();
-  hex.requestRawData();
-   
-  if(craft.rawDataAvailable() > 0) {
-    Serial.print("CRAFT ");
-    while(craft.rawDataAvailable() > 0) {
-      int data = craft.rawDataRead();
-      Serial.print(data);
-      Serial.print(" ");
-    }
-    Serial.println("");
+
+  Serial.println("");
+
+  trillBar.requestRawData();
+  if(trillBar.rawDataAvailable() > 0) {
+    Serial.print("BAR    ");
+    printRawData(trillBar);
   }
 
-  if(squareTrill.rawDataAvailable() > 0) {
+  trillSquare.requestRawData();
+  if(trillSquare.rawDataAvailable() > 0) {
     Serial.print("SQUARE ");
-    while(squareTrill.rawDataAvailable() > 0) {
-      int data = squareTrill.rawDataRead();
-      Serial.print(data);
-      Serial.print(" ");
-    }
-    Serial.println("");
+    printRawData(trillSquare);
   }
 
-  if(bar.rawDataAvailable() > 0) {
-    Serial.print("BAR ");
-    while(bar.rawDataAvailable() > 0) {
-      int data = bar.rawDataRead();
-      Serial.print(data);
-      Serial.print(" ");
-    }
-    Serial.println("");
+  trillCraft.requestRawData();
+  if(trillCraft.rawDataAvailable() > 0) {
+    Serial.print("CRAFT  ");
+    printRawData(trillCraft);
   }
 
-  if(hex.rawDataAvailable() > 0) {
-    Serial.print("HEX ");
-    while(hex.rawDataAvailable() > 0) {
-      int data = hex.rawDataRead();
-      Serial.print(data);
-      Serial.print(" ");
-    }
-    Serial.println("");
+  trillRing.requestRawData();
+  if(trillRing.rawDataAvailable() > 0) {
+    Serial.print("RING  ");
+    printRawData(trillRing);
   }
+
+  trillHex.requestRawData();
+  if(trillHex.rawDataAvailable() > 0) {
+    Serial.print("HEX    ");
+    printRawData(trillHex);
+  }
+
+  printLine(156, '_');
 }
