e40e28c7f161814d689e1fd93313d824a916abd0
Robert Jack <robert.h.jack@gmail.com>
2022-04-13 14:28:06 +0100

    Added two Trill Flex specific examples

---
+++ examples/flex-print-raw/flex-print-raw.ino
@@ -0,0 +1,61 @@
+/*
+   ____  _____ _        _
+   | __ )| ____| |      / \
+   |  _ \|  _| | |     / _ \
+   | |_) | |___| |___ / ___ \
+   |____/|_____|_____/_/   \_\
+http://bela.io
+
+\example flex-print-raw
+
+Trill Flex Print Slider
+=======================
+
+This is an example of how to communicate with the Trill Flex
+sensor using the Trill Arduino library.
+
+The sensor is set to Differential mode and readings from each of the
+capacitive connections on the sensor are printed to the serial port.
+
+You can find our Processing library for visualising here:
+https://github.com/BelaPlatform/trill-processing-library/
+The accompanying Processing sketch, `TrillFlexRaw.pde`, listens for
+touch information on the Arduino serial port* and displays it in a
+render of a each of the channels of Trill Flex.
+ */
+
+#include <Trill.h>
+
+Trill trillSensor;
+
+void setup() {
+	// put your setup code here, to run once:
+	Serial.begin(115200);
+	int ret = trillSensor.setup(Trill::TRILL_FLEX);
+	if(ret != 0) {
+		Serial.println("failed to initialise trillSensor");
+		Serial.print("Error code: ");
+		Serial.println(ret);
+	}
+  // trill flex prefers a prescaler value of 4
+  trillSensor.setPrescaler(4);
+}
+
+void loop() {
+	// put your main code here, to run repeatedly:
+	delay(100);
+	trillSensor.requestRawData();
+
+	while(trillSensor.rawDataAvailable() > 0) {
+		int data = trillSensor.rawDataRead();
+		if(data < 1000)
+			Serial.print(0);
+		if(data < 100)
+			Serial.print(0);
+		if(data < 10)
+			Serial.print(0);
+		Serial.print(data);
+		Serial.print(" ");
+	}
+	Serial.println("");
+}
---
+++ examples/flex-print-slider/flex-print-slider.ino
@@ -0,0 +1,73 @@
+/*
+ ____  _____ _        _
+| __ )| ____| |      / \
+|  _ \|  _| | |     / _ \
+| |_) | |___| |___ / ___ \
+|____/|_____|_____/_/   \_\
+http://bela.io
+
+\example flex-print-slider
+
+Trill Flex Print Slider
+=======================
+
+This is an example of how to communicate with the Trill Flex
+sensor using the Trill Arduino library.
+
+The Trill Flex sensor is read in Differential mode by default. This is
+good for when you are working with custom designed Flex PCBs.
+See the `flex-print-raw` example to read each sensor channel independently.
+
+In this example we will use the sensor in Centroid mode to see
+touch location and size of touches on the default Flexible Bar sensor.
+Touchese will be printed to the serial port for each of the 5 different
+simultaneous touches.
+
+The accompanying Processing sketch, `TrillFlexSlider.pde`, listens for
+touch information on the Arduino serial port* and displays it in a
+render of a Trill Flex.
+
+*NOTE: you may need to update the Processing port number (gPortNumber)
+to match that of your Arduino.
+*/
+
+#include <Trill.h>
+
+Trill trillSensor;
+boolean touchActive = false;
+
+void setup() {
+  // Initialise serial and touch sensor
+  Serial.begin(115200);
+  int ret = trillSensor.setup(Trill::TRILL_FLEX);
+  if(ret != 0) {
+    Serial.println("failed to initialise trillSensor");
+    Serial.print("Error code: ");
+    Serial.println(ret);
+  }
+  // Set the sensor into Centroid mode
+  // The default for Trill Flex is Differential mode
+  trillSensor.setMode(Trill::CENTROID);
+}
+
+void loop() {
+  // Read 20 times per second
+  delay(50);
+  trillSensor.read();
+
+  if(trillSensor.getNumTouches() > 0) {
+    for(int i = 0; i < trillSensor.getNumTouches(); i++) {
+        Serial.print(trillSensor.touchLocation(i));
+        Serial.print(" ");
+        Serial.print(trillSensor.touchSize(i));
+        Serial.print(" ");
+    }
+    Serial.println("");
+    touchActive = true;
+  }
+  else if(touchActive) {
+    // Print a single line when touch goes off
+    Serial.println("0 0");
+    touchActive = false;
+  }
+}
